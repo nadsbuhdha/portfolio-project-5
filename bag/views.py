43b46106a7b4dc41e7e49c5621d695e1fdb9354e
nadsbuhdha <nadsbuhdha@gmail.com>
2022-07-23 14:34:33 +0000

    added add to bag

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from products.models import Product
 
 def view_bag(request):
     """
     A view that renders the bag page:
     """
     return render(request, 'bag/bag.html')
+
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    product = get_object_or_404(Product, pk=item_id)
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+        messages.success(request,
+                         f'Updated {product.name} quantity to {bag[item_id]}')
+    else:
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
