acf580ab19fac0267bf9de7aedf42f067cde5648
nadsbuhdha <nadsbuhdha@gmail.com>
2022-07-23 18:13:14 +0000

    added sizes functionality

--- products/models.py
+++ products/models.py
@@ -44,6 +44,7 @@ class Product(models.Model):
     description = models.TextField()
     brand = models.ForeignKey('Brand', null=True, blank=True, on_delete=models.SET_NULL)
     gender = models.CharField(choices=GENDER, max_length=10,default='u')
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
