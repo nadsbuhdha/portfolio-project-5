3b66c0dbfbdaae72ad67bb2a18a971db0c76b3dc
nadsbuhdha <nadsbuhdha@gmail.com>
2022-07-27 20:47:28 +0000

    updated toast messagiing

--- bag/views.py
+++ bag/views.py
@@ -26,15 +26,19 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size} {product.name} to your bag')
+                
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size} {product.name} to your bag')
+            
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request,
-                            f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
@@ -56,10 +60,12 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed size {size} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
