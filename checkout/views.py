8875cf9c6535786c1150d573553c9a7cf5e4be69
nadsbuhdha <nadsbuhdha@gmail.com>
2022-07-29 12:20:25 +0000

    added stripe and stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -11,7 +11,8 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form' : order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LADABADMfjFKo77lLFhiREBU980UGGcU6sw43e75qFFFaXYwiaYlBkrjbVNG7mWAOy9awYPhnFqGRsmCZ12fG2W00t9o01UUr'
     }
 
     return render(request, template, context)
