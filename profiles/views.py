6cb0e1c766026329879377adead10f99ee079369
nadsbuhdha <nadsbuhdha@gmail.com>
2022-08-04 16:22:07 +0000

    fixed colour on dropdown country box

--- profiles/views.py
+++ profiles/views.py
@@ -7,13 +7,21 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'orders': orders
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
