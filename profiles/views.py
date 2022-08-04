e5915fb778d16781adb275092af88370d19c7d6b
nadsbuhdha <nadsbuhdha@gmail.com>
2022-08-04 15:31:05 +0000

    added forms to user profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .forms import UserProfileForm
+
+from .models import UserProfile
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders
+    }
 
     return render(request, template, context)
