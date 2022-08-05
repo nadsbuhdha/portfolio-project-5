892269610572affdbadebbd48f0a7d1a8fea6494
nadsbuhdha <nadsbuhdha@gmail.com>
2022-08-05 14:34:04 +0000

    added add products view

--- products/forms.py
+++ products/forms.py
@@ -13,9 +13,9 @@ class ProductForm(forms.ModelForm):
         categories = Category.objects.all()
         brands = Brand.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-        brand_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
+        # brand_friendly_names = [(b.id, b.get_friendly_name()) for b in brands]
 
         self.fields['category'].choices = friendly_names
-        self.fields['brand'].choices = brand_friendly_names
+        # self.fields['brand'].choices = brand_friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
