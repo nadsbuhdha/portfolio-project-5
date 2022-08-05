c43ca90fe0a1f5c2534ee0895df62b289efabb05
nadsbuhdha <nadsbuhdha@gmail.com>
2022-08-05 15:51:29 +0000

    added add edit and delete functionality to super user

--- profiles/views.py
+++ profiles/views.py
@@ -9,14 +9,15 @@ def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
