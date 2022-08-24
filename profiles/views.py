df8133c2dd1d2ea44202abc1856ed495d2d35025
nadsbuhdha <nadsbuhdha@gmail.com>
2022-08-24 19:22:09 +0000

    fix python pep8 errors

--- profiles/views.py
+++ profiles/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render, get_object_or_404, redirect, reverse
-from .forms import UserProfileForm
+""" profile views """
+
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import Http404
 from django.contrib import messages
-from products.models import Product
-from checkout.models import Order
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from products.models import Product
+from checkout.models import Order
+from .forms import UserProfileForm
 
 
 from .models import UserProfile, Favourites
+
 
 @login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
-
 
     try:
         favourites = Favourites.objects.filter(user=request.user.id)[0]
@@ -23,7 +25,7 @@ def profile(request):
     else:
         favourites_items = favourites.product.all()
 
-    if not favourites_items :
+    if not favourites_items:
         messages.info(request, 'Your favourites is empty!')
 
     if request.method == 'POST':
@@ -32,7 +34,8 @@ def profile(request):
             form.save()
             messages.success(request, 'Profile updated successfully')
         else:
-            messages.error(request, 'Update failed. Please ensure the form is valid.')
+            messages.error(request, 'Update failed. Please ensure \
+                 the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -42,20 +45,20 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
-        'favourites_items':favourites_items
+        'favourites_items': favourites_items
     }
 
     return render(request, template, context)
 
 
-
 def order_history(request, order_number):
+    """ order history """
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.info(
-        request, (
-            f'This is a past confirmation for order number {order_number}.'
-                'A confirmation email was sent on the order date.'))
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
 
     template = 'checkout/checkout_success.html'
     context = {
@@ -64,7 +67,6 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
-
 
 
 @login_required
